f6f4e440801df13b044dd1ff263776b405ba05bc
Thomas Parker <parkerthomas976@gmail.com>
2017-11-14 15:18:57 -0800

    Switched message from 'hello world' to 'gotta chat'em up'

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello earth');
+  res.send('gotta chat\'em up');
 }); 
 
 app.listen(process.env.PORT || 3000)
