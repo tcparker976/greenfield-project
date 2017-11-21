af12c7f6d61b78ee9e28ef9103e2c294b02729c3
David Kumarbai <cygnidavid@gmail.com>
2017-11-20 17:29:18 -0800

    add new node packages, components, refactor app.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,14 +3,18 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './Home.jsx';
 import Game from './Game.jsx';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';
 import io from 'socket.io-client';
 
 const App = () => {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={Home} />
+        <Route path='/' component={Home} />
         <Route path='/game/:gameid' component={Game} />
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
       </Switch>
     </div>
   );
