6eda47d949d5f1379cf11fb339ea495c2a1656f2
James Terry <jamesnorthterry@gmail.com>
2017-11-26 12:02:38 -0800

    partial implementation of first real test

--- jestSample.test.js
+++ jestSample.test.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
+import ShallowRenderer from 'react-test-renderer/shallow';
+import Chat from './client/src/components/Chat.jsx'
+
 describe('add', () => {
   it('should be true', () => {
     expect(true).toBe(true);
   });
+});
+
+test('chatInput tracks input field changes', () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<Chat />);
+  const result = renderer.getRenderOutput();
+
+  expect(result.type).toBe('div');
 });
